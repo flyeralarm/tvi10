8a1ce4a24c381efadac0c99eafd85f4b131497a0
Stefan Meissner <s.meissner@flyeralarm.de>
2016-09-24 01:49:27 +0200

    PDF Generation

--- src/main/java/com/flyeralarm/tools/tvi10gen/StripBuilder.java
+++ src/main/java/com/flyeralarm/tools/tvi10gen/StripBuilder.java
@@ -3,12 +3,13 @@ package com.flyeralarm.tools.tvi10gen;
 import com.flyeralarm.tools.tvi10gen.model.Patch;
 import com.lowagie.text.*;
 import com.lowagie.text.pdf.*;
-import org.bouncycastle.crypto.tls.ByteQueue;
+import org.apache.commons.io.IOUtils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,73 +17,220 @@ import java.util.List;
  */
 public class StripBuilder {
 
-    private final float FONT_SIZE = 4;
+    private static final String RES_FONT_REGULAR = "/com/flyeralarm/tools/tvi10gen/OpenSans-Regular.ttf";
 
-    private final float BORDER_TOP = mm2dtp(2);
+    private static final String RES_FONT_BOLD = "/com/flyeralarm/tools/tvi10gen/OpenSans-Bold.ttf";
 
-    private final float BORDER_BOTTOM = mm2dtp(2);
+    private static final float FONT_SIZE = 4;
 
-    private final float PATCH_WIDTH = mm2dtp(5);
+    private static final float BORDER_TOP = mm2dtp(2);
 
-    private final float PATCH_HEIGHT = mm2dtp(5);
+    private static final float BORDER_BOTTOM = mm2dtp(2);
+
+    private static final float PATCH_WIDTH = mm2dtp(5);
+
+    private static final float PATCH_HEIGHT = mm2dtp(6);
+
+    private final BaseFont fontRegular;
+
+    private final BaseFont fontBold;
 
     /**
      * Default constructor.
      */
-    public StripBuilder() {
+    public StripBuilder() throws IOException, DocumentException {
+        byte[] bytes;
+
+        bytes = IOUtils.toByteArray(StripBuilder.class.getResourceAsStream(RES_FONT_REGULAR));
+        fontRegular = BaseFont.createFont("regular.ttf", BaseFont.WINANSI, BaseFont.EMBEDDED, true, bytes, null);
+
+        bytes = IOUtils.toByteArray(StripBuilder.class.getResourceAsStream(RES_FONT_BOLD));
+        fontBold = BaseFont.createFont("bold.ttf", BaseFont.WINANSI, BaseFont.EMBEDDED, true, bytes, null);
     }
 
-
+    /**
+     * Build the TVI Strip PDF.
+     *
+     * @param targets    The targets as list of Patch objects.
+     * @param code       The code as list of Patch objects.
+     * @param codeValue  The code as String.
+     * @param targetFile The file where the PDF has to be written.
+     */
     public void build(Patch[] targets, Patch[] code, String codeValue, File targetFile) throws IOException, DocumentException {
         float stripHeight = PATCH_HEIGHT + BORDER_BOTTOM + BORDER_TOP;
         float stripWidth = 0;
 
         // create page
-        Document document = new Document(PageSize.A3);
+        Document document = new Document();
         PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(targetFile));
         document.open();
 
         PdfContentByte cb = writer.getDirectContent();
+        List<PdfTemplate> tplParts = new ArrayList<>(10);
 
         // draw target part
-        PdfTemplate tplTarget = drawTarget(targets, cb);
-        stripWidth += tplTarget.getWidth();
-
-        // draw code part
-        PdfTemplate tplCode = drawCode(code, codeValue, cb);
-        stripWidth += tplCode.getWidth();
+        tplParts.add(drawStart(cb));
+        tplParts.add(drawTarget(targets, cb));
+        tplParts.add(drawCode(code, codeValue, cb));
+        tplParts.add(drawStop(cb));
+        tplParts.add(drawSplash(cb));
 
         // create final strip
-        PdfTemplate tplStrip = cb.createTemplate(stripWidth * 100, stripHeight * 100);
+        for (PdfTemplate tplPart : tplParts) {
+            stripWidth += tplPart.getWidth();
+        }
+
+        PdfTemplate tplStrip = cb.createTemplate(stripWidth, stripHeight);
 
         Rectangle rectBg = new Rectangle(0, 0, stripWidth, stripHeight);
-        rectBg.setBackgroundColor(new CMYKColor(0f, 0f, 0f, 0.05f));
+        rectBg.setBackgroundColor(new CMYKColor(0f, 0f, 0f, 0f));
         tplStrip.rectangle(rectBg);
 
-        tplStrip.addTemplate(tplTarget, 0, 0);
-        tplStrip.addTemplate(tplCode, tplTarget.getWidth(), 0);
+        float pntPosition = 0;
+
+        for (PdfTemplate tplPart : tplParts) {
+            tplStrip.addTemplate(tplPart, pntPosition, 0);
+            pntPosition += tplPart.getWidth();
+        }
+
+        document.setPageSize(new Rectangle(0, 0, stripWidth, stripHeight));
+        document.newPage();
+
+        cb.addTemplate(tplStrip, 0, 0);
 
         // close document
-        cb.addTemplate(tplStrip, 20, 1000);
         document.close();
     }
 
     /**
-     * Create the code part of the strip.
-     * @param codes The code patches to be drawn.
-     * @param codeValue The code value as String.
+     * Draw the splash part.
      * @param cb The PdfContentByte.
+     * @return The PdfTemplate containing the splash part.
+     */
+    private PdfTemplate drawSplash(PdfContentByte cb) {
+        float stripWidth = mm2dtp(25);
+        float stripHeight = PATCH_HEIGHT + BORDER_BOTTOM + BORDER_TOP;
+
+        PdfTemplate tplSplash = cb.createTemplate(stripWidth, stripHeight);
+
+        float refLine = BORDER_BOTTOM + PATCH_HEIGHT * 0.5f;
+
+        // strip name
+        tplSplash.beginText();
+        tplSplash.setFontAndSize(fontBold, 6);
+        tplSplash.showTextAligned(
+                PdfContentByte.ALIGN_LEFT,
+                "FLYERALARM tvi 10",
+                mm2dtp(2),
+                refLine,
+                0
+        );
+        tplSplash.endText();
+
+        // eci note
+        tplSplash.beginText();
+        tplSplash.setFontAndSize(fontRegular, 4);
+        tplSplash.showTextAligned(
+                PdfContentByte.ALIGN_LEFT,
+                "ECI/bvdm tvi 10 (i1) compatible",
+                mm2dtp(2),
+                refLine - mm2dtp(2f),
+                0
+        );
+        tplSplash.endText();
+
+
+        return tplSplash;
+    }
+
+    /**
+     * Create the start part.
+     *
+     * @param cb The PdfContentByte
+     * @return The PdfTemplate containing the start part.
+     */
+    private PdfTemplate drawStart(PdfContentByte cb) {
+        float stripWidth = mm2dtp(2);
+        float stripHeight = PATCH_HEIGHT + BORDER_BOTTOM + BORDER_TOP;
+
+        PdfTemplate tplLeftBorder = cb.createTemplate(stripWidth, stripHeight);
+
+        // draw line
+        tplLeftBorder.setCMYKColorStrokeF(0f, 0f, 0f, 1.0f);
+        tplLeftBorder.moveTo(stripWidth, BORDER_BOTTOM);
+        tplLeftBorder.lineTo(stripWidth, BORDER_BOTTOM + PATCH_HEIGHT);
+        tplLeftBorder.stroke();
+
+        // draw "Start"
+        tplLeftBorder.beginText();
+        tplLeftBorder.setCMYKColorFillF(0f, 1f, 1f, 0f);
+        tplLeftBorder.setFontAndSize(fontBold, FONT_SIZE);
+        tplLeftBorder.showTextAligned(
+                PdfContentByte.ALIGN_CENTER,
+                "Start",
+                stripWidth - 4.5f,
+                BORDER_BOTTOM + PATCH_HEIGHT * 0.5f,
+                -90
+        );
+        tplLeftBorder.endText();
+
+        return tplLeftBorder;
+    }
+
+    /**
+     * Create the stop part.
+     *
+     * @param cb The PdfContentByte
+     * @return The PdfTemplate containing the stop part.
+     */
+    private PdfTemplate drawStop(PdfContentByte cb) {
+        float stripWidth = mm2dtp(2);
+        float stripHeight = PATCH_HEIGHT + BORDER_BOTTOM + BORDER_TOP;
+
+        PdfTemplate tplLeftBorder = cb.createTemplate(stripWidth, stripHeight);
+
+        // draw line
+        tplLeftBorder.setCMYKColorStrokeF(0f, 0f, 0f, 1.0f);
+        tplLeftBorder.moveTo(0, BORDER_BOTTOM);
+        tplLeftBorder.lineTo(0, BORDER_BOTTOM + PATCH_HEIGHT);
+        tplLeftBorder.stroke();
+
+        // draw "Stop"
+        tplLeftBorder.beginText();
+        tplLeftBorder.setCMYKColorFillF(0f, 1f, 1f, 0f);
+        tplLeftBorder.setFontAndSize(fontBold, FONT_SIZE);
+        tplLeftBorder.showTextAligned(
+                PdfContentByte.ALIGN_CENTER,
+                "Stop",
+                2.5f,
+                BORDER_BOTTOM + PATCH_HEIGHT * 0.5f,
+                -90
+        );
+        tplLeftBorder.endText();
+
+        return tplLeftBorder;
+    }
+
+    /**
+     * Create the code part of the strip.
+     *
+     * @param codes     The code patches to be drawn.
+     * @param codeValue The code value as String.
+     * @param cb        The PdfContentByte.
      * @return The PdfTemplate containing the codes part.
      */
     private PdfTemplate drawCode(Patch[] codes, String codeValue, PdfContentByte cb) throws IOException, DocumentException {
-        float stripWidth = PATCH_WIDTH * codes.length;
+        List<Patch> lstCodes = new ArrayList<>(Arrays.asList(codes));
+        lstCodes.add(new Patch(0, 0, 0, 0));
+
+        float stripWidth = PATCH_WIDTH * lstCodes.size();
         float stripHeight = PATCH_HEIGHT + BORDER_BOTTOM + BORDER_TOP;
 
         PdfTemplate tplCode = cb.createTemplate(stripWidth, stripHeight);
 
         // draw patches
-        for(int i = 0; i < codes.length; i ++) {
-            Patch patch = codes[i];
+        for (int i = 0; i < lstCodes.size(); i++) {
+            Patch patch = lstCodes.get(i);
 
             // draw patch
             CMYKColor cmykColor = new CMYKColor(
@@ -104,16 +252,26 @@ public class StripBuilder {
 
         // value
         tplCode.beginText();
-        BaseFont bf = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
-        tplCode.setFontAndSize(bf, FONT_SIZE);
+        tplCode.setFontAndSize(fontRegular, FONT_SIZE);
         tplCode.showTextAligned(
                 PdfContentByte.ALIGN_LEFT,
                 "Code: " + codeValue,
                 0,
-                0 - bf.getDescentPoint("WgKyJ", FONT_SIZE),
+                BORDER_BOTTOM + PATCH_HEIGHT + 1.5f,
                 0
         );
         tplCode.endText();
+
+        // draw lines
+        tplCode.setCMYKColorStrokeF(0f, 0f, 0f, 1.0f);
+        tplCode.moveTo(0, BORDER_BOTTOM);
+        tplCode.lineTo(0, BORDER_BOTTOM + PATCH_HEIGHT);
+        tplCode.stroke();
+
+        tplCode.setCMYKColorStrokeF(0f, 0f, 0f, 1.0f);
+        tplCode.moveTo(stripWidth, BORDER_BOTTOM);
+        tplCode.lineTo(stripWidth, BORDER_BOTTOM + PATCH_HEIGHT);
+        tplCode.stroke();
 
         // return code part
         return tplCode;
@@ -121,19 +279,24 @@ public class StripBuilder {
 
     /**
      * Create the target part of the strip.
+     *
      * @param targets The target patches to be drawn.
-     * @param cb The PdfcontentByte.
+     * @param cb      The PdfcontentByte.
      * @return The PdfTemplate containing the target part
      */
     private PdfTemplate drawTarget(Patch[] targets, PdfContentByte cb) throws IOException, DocumentException {
-        float stripWidth = PATCH_WIDTH * (targets.length + 2);
+        List<Patch> lstTargets = new ArrayList<>(Arrays.asList(targets));
+        lstTargets.add(0, new Patch(0, 0, 0, 0));
+        lstTargets.add(new Patch(0, 0, 0, 0));
+
+        float stripWidth = PATCH_WIDTH * lstTargets.size();
         float stripHeight = PATCH_HEIGHT + BORDER_BOTTOM + BORDER_TOP;
 
         PdfTemplate tplTarget = cb.createTemplate(stripWidth, stripHeight);
 
         // draw patches
-        for(int i = 0; i < targets.length; i ++) {
-            Patch patch = targets[i];
+        for (int i = 0; i < lstTargets.size(); i++) {
+            Patch patch = lstTargets.get(i);
 
             // draw patch
             CMYKColor cmykColor = new CMYKColor(
@@ -144,40 +307,89 @@ public class StripBuilder {
             );
 
             Rectangle rectPatch = new Rectangle(
-                    PATCH_WIDTH * (i + 1),
+                    PATCH_WIDTH * i,
                     BORDER_BOTTOM,
-                    PATCH_WIDTH * (i + 2),
+                    PATCH_WIDTH * (i + 1),
                     PATCH_HEIGHT + BORDER_BOTTOM
             );
             rectPatch.setBackgroundColor(cmykColor);
             tplTarget.rectangle(rectPatch);
 
             // draw name
-            tplTarget.beginText();
-            BaseFont bf = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
-            tplTarget.setFontAndSize(bf, FONT_SIZE);
-            tplTarget.showTextAligned(
-                    PdfContentByte.ALIGN_CENTER,
-                    patch.getName(),
-                    PATCH_WIDTH * (i + 1) + PATCH_WIDTH * 0.5f,
-                    PATCH_HEIGHT + BORDER_BOTTOM - bf.getDescentPoint("WgKyJ", FONT_SIZE),
-                    0
-            );
-            tplTarget.endText();
+            if (i > 0 && i < lstTargets.size() - 1) {
+                tplTarget.beginText();
+                tplTarget.setFontAndSize(fontRegular, FONT_SIZE);
+                tplTarget.showTextAligned(
+                        PdfContentByte.ALIGN_CENTER,
+                        patch.getName(),
+                        PATCH_WIDTH * i + PATCH_WIDTH * 0.5f,
+                        PATCH_HEIGHT + BORDER_BOTTOM + 1.5f,
+                        0
+                );
+                tplTarget.endText();
+            }
         }
 
-        // author
+        // draw lines
+        tplTarget.setCMYKColorStrokeF(0f, 0f, 0f, 1.0f);
+        tplTarget.moveTo(0, BORDER_BOTTOM);
+        tplTarget.lineTo(0, BORDER_BOTTOM + PATCH_HEIGHT);
+        tplTarget.stroke();
+
+        tplTarget.setCMYKColorStrokeF(0f, 0f, 0f, 1.0f);
+        tplTarget.moveTo(stripWidth, BORDER_BOTTOM);
+        tplTarget.lineTo(stripWidth, BORDER_BOTTOM + PATCH_HEIGHT);
+        tplTarget.stroke();
+
+        // strip name
+        String name = "FLYERALARM tvi 10 (v 1.0)  •  ";
+
         tplTarget.beginText();
-        BaseFont bf = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
-        tplTarget.setFontAndSize(bf, FONT_SIZE);
+        tplTarget.setFontAndSize(fontBold, FONT_SIZE);
         tplTarget.showTextAligned(
                 PdfContentByte.ALIGN_LEFT,
-                "ricebean.net/tvi10",
-                0,
-                0 - bf.getDescentPoint("WgKyJ", FONT_SIZE),
+                name,
+                PATCH_WIDTH,
+                1.5f,
                 0
         );
         tplTarget.endText();
+
+        // strip text
+        String text = "Print Control Strip containing the process colour solids as defined in" +
+                " ISO 12647-2 as well as panels describing the tone value increase curve in 10 percent steps.";
+
+        tplTarget.beginText();
+        tplTarget.setFontAndSize(fontRegular, FONT_SIZE);
+        tplTarget.showTextAligned(
+                PdfContentByte.ALIGN_LEFT,
+                text,
+                fontBold.getWidthPoint(name, FONT_SIZE) + PATCH_WIDTH,
+                1.5f,
+                0
+        );
+        tplTarget.endText();
+
+        // tvi 10 end text
+        tplTarget.beginText();
+        tplTarget.setCMYKColorFillF(0f, 1f, 1f, 0f);
+        tplTarget.setFontAndSize(fontBold, FONT_SIZE);
+        tplTarget.showTextAligned(
+                PdfContentByte.ALIGN_RIGHT,
+                "tvi 10 END",
+                stripWidth - 2,
+                1.5f,
+                0
+        );
+        tplTarget.endText();
+
+        tplTarget.setLineWidth(0.5f);
+        tplTarget.setCMYKColorStrokeF(0f, 1f, 1f, 0f);
+        tplTarget.moveTo(stripWidth - 0.25f, BORDER_BOTTOM - 1f);
+        tplTarget.lineTo(stripWidth - 0.25f, 1f);
+        tplTarget.lineTo(stripWidth - 2f, 1f);
+        tplTarget.stroke();
+
 
         // return measurement
         return tplTarget;
@@ -185,10 +397,11 @@ public class StripBuilder {
 
     /**
      * Helper method for converting millimeter into dtp point.
+     *
      * @param mm The value in millimeter to be converted.
      * @return The converted value in dtp points.
      */
-    private float mm2dtp(float mm) {
+    private static float mm2dtp(float mm) {
         return mm / 25.4f * 72f;
     }
 }
